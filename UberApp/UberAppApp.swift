06d70394cc9cf5dfd6050bb4c0977e82743aaac1
CoderEslam <eslamghazy600@gmail.com>
2023-08-18 22:15:37 +0200

    Initial Commit

---
+++ UberApp/UberAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberAppApp.swift
+//  UberApp
+//
+//  Created by Eslam Ghazy on 18/8/23.
+//
+
+import SwiftUI
+
+@main
+struct UberAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
